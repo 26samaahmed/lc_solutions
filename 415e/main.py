5132342812fa08b5e204e5ecd4234cda47143146
Sama Ahmed <111910374+26samaahmed@users.noreply.github.com>
2023-07-06 23:02:24 -0700

    Create main.py

---
+++ 415e/main.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        sum = int(num1) + int(num2)
+        return str(sum)
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
