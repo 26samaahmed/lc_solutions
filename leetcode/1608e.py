60a2e927a2bd3444f9aa14ee6fdeeefdb1c49b69
Sama Ahmed <26samahmed@csu.fullerton.edu>
2024-05-26 23:07:28 -0700

    Create 1608e.py

---
+++ leetcode/1608e.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def specialArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        counter = 0
+        for x in range(len(nums)):
+            for i in nums:
+                if i >= x + 1:
+                    counter += 1
+            if counter == x + 1:
+                return counter
+            else:
+                counter = 0
+        if counter == 0:
+            return -1
+
+# if x is equal to a certain number
+# Loop through the list and check if we have x amount of numbers that are >= x
+# technically the maximum x can be is the length of the the list, if x == len(list), then there must be x numbers bigger than or equal to x
